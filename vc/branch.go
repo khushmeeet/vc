edbcd8b956e87f639fa654d84e7d4eb25c79f48e
khushmeeet <khushmeetsingh1996@gmail.com>
2021-05-09 03:36:27 +0530

    Implemented basic branch ref creation

---
+++ vc/branch.go
@@ -0,0 +1,25 @@
+package vc
+
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+)
+
+func Branch(name, oid string) {
+	err := createBranch(name, oid)
+	if err != nil {
+		log.Fatalf("error creating branch - %v\n", err)
+	}
+
+	fmt.Printf("Branch %v created at %v\n", name, oid)
+}
+
+func createBranch(name, oid string) error {
+	err := updateRef(filepath.Join("refs", "heads", name), oid)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
